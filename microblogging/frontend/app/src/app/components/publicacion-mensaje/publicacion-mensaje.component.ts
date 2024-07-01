8a0f087581c9c4606d678512accec590016884d6
Augusto_Mancuso <a.mancuso@alumno.um.edu.ar>
2024-07-01 19:39:51 -0300

    Fix

--- microblogging/frontend/app/src/app/components/publicacion-mensaje/publicacion-mensaje.component.ts
+++ microblogging/frontend/app/src/app/components/publicacion-mensaje/publicacion-mensaje.component.ts
@@ -11,11 +11,11 @@ import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, signal} from '@angular/core';
 import {MatChipEditedEvent, MatChipInputEvent, MatChipsModule} from '@angular/material/chips';
 import {
-    FormBuilder,
-    FormGroup,
-    FormsModule,
-    ReactiveFormsModule,
-    Validators
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators
 } from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {BarraDeBusquedaComponent} from "../barra-de-busqueda/barra-de-busqueda.component";
@@ -29,177 +29,173 @@ import {NuevoMensaje} from "../../models/nuevo-mensaje.model";
 
 
 @Component({
-    selector: 'app-publicacion-mensaje',
-    standalone: true,
-    imports: [
-        MatCardModule,
-        MatChipsModule,
-        MatProgressBarModule,
-        NgFor,
-        NgIf,
-        MatButtonModule,
-        MatMenuModule,
-        MatCheckboxModule,
-        MatIconModule,
-        MatFormFieldModule,
-        FormsModule,
-        MatInputModule,
-        ReactiveFormsModule,
-        BarraDeBusquedaComponent,
-        MatDialogActions,
-        MatDialogContent,
-        MatDialogClose,
+  selector: 'app-publicacion-mensaje',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatChipsModule,
+    MatProgressBarModule,
+    NgFor,
+    NgIf,
+    MatButtonModule,
+    MatMenuModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    BarraDeBusquedaComponent,
+    MatDialogActions,
+    MatDialogContent,
+    MatDialogClose,
 
 
-    ],
-    templateUrl: './publicacion-mensaje.component.html',
-    styleUrl: './publicacion-mensaje.component.css',
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  ],
+  templateUrl: './publicacion-mensaje.component.html',
+  styleUrl: './publicacion-mensaje.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PublicacionMensajeComponent {
 
-    readonly addOnBlur = true;
-    readonly separatorKeysCodes = [ENTER, COMMA] as const;
-    readonly etiquetas = signal<any[]>([]);
-    readonly announcer = inject(LiveAnnouncer);
-    readonly dialogBusqueda = inject(MatDialog);
-    readonly dialogMensajePrivado = inject(MatDialog);
-    form: FormGroup;
-    usuariosMencionados: Usuario[] = [];
-    usuarioMensajePrivado: Usuario[] = [];
-    private subscription: Subscription | null = null;
+  readonly addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  readonly etiquetas = signal<(any | string)[]>([]);
+  readonly announcer = inject(LiveAnnouncer);
+  readonly dialogBusqueda = inject(MatDialog);
+  readonly dialogMensajePrivado = inject(MatDialog);
+  form: FormGroup;
+  usuariosMencionados: Usuario[] = [];
+  usuarioMensajePrivado: Usuario[] = [];
+  private subscription: Subscription | null = null;
 
-    constructor(
-        private fb: FormBuilder,
-        private usuarioSvc: UsuarioService,
-        private changeDetectorRef: ChangeDetectorRef
-    ) {
-        this.form = this.fb.group({
-            textoFormControl: ['', [Validators.required, Validators.maxLength(140)]]
-        });
+  constructor(
+    private fb: FormBuilder,
+    private usuarioSvc: UsuarioService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {
+    this.form = this.fb.group({
+      textoFormControl: ['', [Validators.required, Validators.maxLength(140)]]
+    });
+  }
+
+  ngOnInit() {
+    this.subscription = this.usuarioSvc.usuariosMencionados.subscribe(
+      usuarios => {
+        this.usuariosMencionados = usuarios;
+        console.log("USUARIOS CARGADOS: ", this.usuariosMencionados.length, this.usuariosMencionados)
+        this.changeDetectorRef.detectChanges();
+      }
+    );
+    this.subscription = this.usuarioSvc.usuarioMensajePrivado.subscribe(
+      usuarioMsjPrivado => {
+        console.log("USUARIOS CARGADOS: ", this.usuarioMensajePrivado.length, this.usuarioMensajePrivado)
+        this.usuarioMensajePrivado = usuarioMsjPrivado;
+        this.changeDetectorRef.detectChanges();
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
+  }
 
-    ngOnInit() {
-        this.subscription = this.usuarioSvc.usuariosMencionados.subscribe(
-            usuarios => {
-                this.usuariosMencionados = usuarios;
-                console.log("USUARIOS CARGADOS: ", this.usuariosMencionados.length, this.usuariosMencionados)
-                this.changeDetectorRef.detectChanges();
-            }
-        );
-        this.subscription = this.usuarioSvc.usuarioMensajePrivado.subscribe(
-            usuarioMsjPrivado => {
-                console.log("USUARIOS CARGADOS: ", this.usuarioMensajePrivado.length, this.usuarioMensajePrivado)
-                this.usuarioMensajePrivado = usuarioMsjPrivado;
-                this.changeDetectorRef.detectChanges();
-            }
-        )
+
+  addEtiqueta(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    if (value && value) {
+      this.etiquetas.update(etiquetas => [...etiquetas, {valor: "#" + value}]);
     }
+    event.chipInput!.clear();
+  }
 
-    ngOnDestroy() {
-        if (this.subscription) {
-            this.subscription.unsubscribe();
+
+  remove(etiqueta: Etiqueta | Mencion): void {
+    this.etiquetas.update(etiquetas => {
+      const index = etiquetas.indexOf(etiqueta);
+      if (index < 0) {
+        return etiquetas;
+      }
+      etiquetas.splice(index, 1);
+      this.announcer.announce(`Removed ${etiqueta.valor}`);
+      return [...etiquetas];
+    });
+  }
+
+  edit(etiqueta: Etiqueta | Mencion, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+    if (!value) {
+      this.remove(etiqueta);
+      return;
+    }
+    this.etiquetas.update(etiquetas => {
+      const index = etiquetas.indexOf(etiqueta);
+      if (index >= 0) {
+        etiquetas[index].valor = value;
+        return [...etiquetas];
+      }
+      return etiquetas;
+    });
+  }
+
+
+  abrirMenciones() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '40%';
+    const dialogRef = this.dialogBusqueda.open(BusquedaMencionesComponent, dialogConfig);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  abrirMensajePrivado() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '40%';
+    const dialogRef = this.dialogMensajePrivado.open(MensajePrivadoComponent, dialogConfig);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  publicarMensaje() {
+    console.log("Publicando mensaje")
+
+    if (!this.form.valid) {
+      console.log("El formulario no es válido. Por favor, revisa los campos.");
+      alert("El mensaje debe contener al menos texto.")
+    }
+    let destinatarioId = this.usuarioMensajePrivado && this.usuarioMensajePrivado.length > 0 ? Number(this.usuarioMensajePrivado[0].id) : null;
+    let etiquetasValue = this.etiquetas();
+    let etiquetas: string[] = etiquetasValue && etiquetasValue.length > 0 ? this.etiquetas().map(etiqueta => etiqueta.valor) : [];
+    let usuariosMencionadosValue = this.usuariosMencionados;
+    let mencionUsuarioId = usuariosMencionadosValue && usuariosMencionadosValue.length > 0 ? usuariosMencionadosValue.map(usuario => usuario.id) : [];
+    let nuevoMensaje = new NuevoMensaje(
+      Number(localStorage.getItem('idUsuario')),
+      this.form.get('textoFormControl')?.value,
+      destinatarioId,
+      etiquetas,
+      mencionUsuarioId
+    );
+    this.usuarioSvc.publicarMensaje(nuevoMensaje).then(
+      res => {
+        if (res.response == null) {
+          console.log("Mensaje no creado")
+          return;
         }
-    }
-
-
-    addEtiqueta(event: MatChipInputEvent): void {
-        const value = (event.value || '').trim();
-        if (value && value) {
-            this.etiquetas.update(etiquetas => [...etiquetas, {valor: "#" + value}]);
-        }
-        event.chipInput!.clear();
-    }
-
-
-    remove(etiqueta: Etiqueta | Mencion): void {
-        this.etiquetas.update(etiquetas => {
-            const index = etiquetas.indexOf(etiqueta);
-            if (index < 0) {
-                return etiquetas;
-            }
-            etiquetas.splice(index, 1);
-            this.announcer.announce(`Removed ${etiqueta.valor}`);
-            return [...etiquetas];
-        });
-    }
-
-    edit(etiqueta: Etiqueta | Mencion, event: MatChipEditedEvent) {
-        const value = event.value.trim();
-        if (!value) {
-            this.remove(etiqueta);
-            return;
-        }
-        this.etiquetas.update(etiquetas => {
-            const index = etiquetas.indexOf(etiqueta);
-            if (index >= 0) {
-                etiquetas[index].valor = value;
-                return [...etiquetas];
-            }
-            return etiquetas;
-        });
-    }
-
-
-    abrirMenciones() {
-        const dialogConfig = new MatDialogConfig();
-        dialogConfig.width = '40%';
-        const dialogRef = this.dialogBusqueda.open(BusquedaMencionesComponent, dialogConfig);
-        dialogRef.afterClosed().subscribe(result => {
-            console.log(`Dialog result: ${result}`);
-        });
-    }
-
-    abrirMensajePrivado() {
-        const dialogConfig = new MatDialogConfig();
-        dialogConfig.width = '40%';
-        const dialogRef = this.dialogMensajePrivado.open(MensajePrivadoComponent, dialogConfig);
-        dialogRef.afterClosed().subscribe(result => {
-            console.log(`Dialog result: ${result}`);
-        });
-    }
-
-    publicarMensaje() {
-      debugger
-        console.log("Publicando mensaje")
-
-        if (!this.form.valid) {
-            console.log("El formulario no es válido. Por favor, revisa los campos.");
-            return;
-        }
-        let etiquetas: string[] = this.etiquetas().map(etiqueta => etiqueta.valor);
-        let mencionUsuarioId: (string | number)[] = this.usuariosMencionados.map((
-            usuario => usuario.id
-        ))
-        console.log("etiquetas", etiquetas)
-        console.log("menciones usuarios id", mencionUsuarioId)
-        console.log("texto", this.form.get('textoFormControl')?.value)
-        console.log("ID usuario privado", this.usuarioMensajePrivado) // Esta vacio este array ver que onda
-        let nuevoMensaje = new NuevoMensaje(
-            Number(localStorage.getItem('idUsuario')),
-            this.form.get('textoFormControl')?.value,
-            Number(this.usuarioMensajePrivado[0].id),
-            etiquetas,
-            mencionUsuarioId
-        );
-        this.usuarioSvc.publicarMensaje(nuevoMensaje).then(
-          res => {
-            if (res.response == null){
-              console.log("Mensaje no creado")
-              return;
-            }
-            alert("Mensaje Publicado")
-            location.reload();
-          }
-        )
-    }
+        alert("Mensaje Publicado")
+        location.reload();
+      }
+    )
+  }
 }
 
 
 export interface Etiqueta {
-    valor: string;
+  valor: string;
 }
 
 export interface Mencion {
-    valor: string;
+  valor: string;
 }
